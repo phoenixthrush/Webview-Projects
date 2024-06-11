f11a5da6ef4d0a4a9336f0f335c35b399e69ee6d
phoenixthrush <mr.cetto@phoenixthrush.com>
2024-06-11 22:09:35 +0200

    add MacOS icon

--- compile.sh
+++ compile.sh
@@ -1,7 +1,7 @@
 # MacOS
 rm -rf main.app Resources Info.plist
 
-python3 -m nuitka --onefile --include-data-files=site/index.html=index.html --assume-yes-for-downloads --remove-output --macos-create-app-bundle --macos-signed-app-name=com.Phoenixthrush.Keygen --company-name=phoenixthrush main.py
+python3 -m nuitka --include-data-files=site/index.html=index.html --assume-yes-for-downloads --remove-output --macos-create-app-bundle --macos-app-icon=site/assets/icon.png --macos-signed-app-name=com.Phoenixthrush.Keygen --macos-app-name=Keygen --company-name=phoenixthrush main.py
 # --linux-icon=site/assets/icon.ico
 
 rm -rf Info.plist Resources _CodeSignature
