9b80d088d4ec012ea2edb64cacca3119f419173b
phoenixthrush <mr.cetto@phoenixthrush.com>
2024-06-11 21:58:41 +0200

    fix macos

--- compile.sh
+++ compile.sh
@@ -1,7 +1,7 @@
 # MacOS
 rm -rf main.app Resources Info.plist
 
-python3 -m nuitka --onefile --include-data-files=site/index.html=index.html --assume-yes-for-downloads --remove-output --macos-create-app-bundle --macos-signed-app-name=com.Phoenixthrush.Keygen --macos-app-name=Keygen --company-name=phoenixthrush --output-filename=Keygen.app main.py
+python3 -m nuitka --onefile --include-data-files=site/index.html=index.html --assume-yes-for-downloads --remove-output --macos-create-app-bundle --macos-signed-app-name=com.Phoenixthrush.Keygen --company-name=phoenixthrush main.py
 # --linux-icon=site/assets/icon.ico
 
-rm -rf Info.plist Resources
+rm -rf Info.plist Resources _CodeSignature
