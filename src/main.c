3bbc99d3fe8990d4a24e4aa380984e217746fb8b
phoenixthrush <mr.cetto@phoenixthrush.com>
2024-08-26 16:53:36 +0200

    Non working initial commit

---
+++ src/main.c
@@ -0,0 +1,37 @@
+#include "../libs/webview/webview.h"
+#include <stdio.h>
+#include <stddef.h>
+
+#include "../build/html.c"
+
+#ifdef _WIN32
+#include <windows.h>
+#endif
+
+void call_encrypt(const char *id, const char *req, void *arg);
+void call_decrypt(const char *id, const char *req, void *arg);
+void set_directory(const char *id, const char *req, void *arg);
+
+#ifdef _WIN32
+int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrevInst, LPSTR lpCmdLine,
+                   int nCmdShow)
+{
+    (void)hInst;
+    (void)hPrevInst;
+    (void)lpCmdLine;
+    (void)nCmdShow;
+#else
+int main()
+{
+#endif
+    webview_t w = webview_create(0, NULL);
+    webview_set_size(w, 600, 380, WEBVIEW_HINT_FIXED); // 750, 475
+    webview_set_title(w, "Simple XOR Encryption");
+    webview_bind(w, "call_encrypt", call_encrypt, NULL);
+    webview_bind(w, "call_decrypt", call_decrypt, NULL);
+    webview_bind(w, "set_directory", set_directory, NULL);
+    webview_set_html(w, (const char *)site_index_html);
+    webview_run(w);
+    webview_destroy(w);
+    return 0;
+}
