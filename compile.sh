2354e248c4c917af185543eb2900aea02f31ee90
phoenixthrush <mr.cetto@phoenixthrush.com>
2024-08-29 13:56:19 +0200

    mv files into src

--- compile.sh
+++ compile.sh
@@ -1,3 +1,4 @@
+cd src
 xxd -i site/index.html > html.c
 
 cmake -G Ninja -B build -S . -D CMAKE_BUILD_TYPE=Release
@@ -7,3 +8,4 @@ strip ./build/bin/example
 du -hs ./build/bin/example
 
 ./build/bin/example
+cd ..
